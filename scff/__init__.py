f16c486f707023153142a4f88cc1b80d0705e71e
shayanejaz007 <116840105+shayanejaz007@users.noreply.github.com>
2026-02-14 00:13:31 -0500

    Update __init__.py

--- scff/__init__.py
+++ scff/__init__.py
@@ -1,3 +1,6 @@
-"""Federated SCFF - Multi-client Split Learning"""
-from .partition import DataPartitioner, get_client_loader
-from .protocol import ModelState, ClientUpdate, RoundMetadata
+"""SCFF - Self-Contrastive Forward-Forward Learning"""
+from .layers import Conv2d, triangle, standardnorm, L2norm, stdnorm
+from .data import DualAugmentCIFAR10, DualAugmentCIFAR10_test, get_train
+from .model import build_layers_from_config, get_pos_neg_batch_imgcats
+from .trainer import SCFFTrainer, EvaluationConfig
+from .eval import evaluate_model, train_readout, test_readout
